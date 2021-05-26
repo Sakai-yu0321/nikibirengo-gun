2be35602f774c6198adbcc994f658bd2d98466ae
Sakai-yu0321 <on99.yu.sakai@gmail.com>
2021-05-26 15:04:53 +0900

    投稿フォームへのルーティング、コントローラー、ビューの設定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,31 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: []
+
   def index
     @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to :root_path
+      flash[:notice] = "投稿が完了しました"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :text).merge(user_id: current_user.id)
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -6,16 +6,3 @@ FactoryBot.define do
     password_confirmation { password }
   end
 end
-# FactoryBot.define do
-#   factory :user do
-#     nickname              { Faker::Name.initials(number: 2) }
-#     email                 { Faker::Internet.free_email }
-#     password              { '1a' + Faker::Internet.password(min_length: 6) }
-#     password_confirmation { password }
-#     last_name             { 'やマ田' }
-#     first_name            { 'たロ宇' }
-#     kana_last_name        { 'ヤマダ' }
-#     kana_first_name       { 'タロウ' }
-#     birthday              { '2000-01-01' }
-#   end
-# end

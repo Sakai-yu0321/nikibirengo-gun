b78d5b2786e2a18cba19517cd284bd1aa88ac4dc
Sakai-yu0321 <on99.yu.sakai@gmail.com>
2021-05-31 16:48:53 +0900

    flashメッセージにアニメーション追加

---
+++ app/javascript/flash.js
@@ -0,0 +1,6 @@
+$(function(){
+  setTimeout("$('#flash_notice').slideDown('slow')", 100);
+  setTimeout("$('#flash_notice').slideUp('slow')", 2500);
+  setTimeout("$('#flash_alert').slideDown('slow')", 100);
+  setTimeout("$('#flash_alert').slideUp('slow')", 2500);
+});

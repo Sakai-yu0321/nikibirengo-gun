0095017dfc00b851e7c0842ff8ecf32d80818a66
Sakai-yu0321 <on99.yu.sakai@gmail.com>
2021-06-02 15:28:58 +0900

    いいね機能非同期（途中）

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,8 +2,18 @@ class LikesController < ApplicationController
   before_action :authenticate_user!, only: [:create, :destroy]
 
   def create
-    Like.create(user_id: current_user.id, post_id: params[:post_id])
-    redirect_to post_path(params[:post_id]), notice:"いいねをしました"
+    @like = Like.new(user_id: current_user.id, post_id: params[:post_id])
+    # redirect_to post_path(params[:post_id]), notice:"いいねをしました"
+
+    respond_to do |format|
+      if @like.save
+        format.html { redirect_to post_path(params[:post_id]), notice:"いいねをしました" }
+        format.json { render json: {status: 'success', like: @like, counts: Like.where(post_id: @like.post_id).count, liked: true} }
+      else
+        format.html { render 'posts/show' }
+        format.json { render json: @like.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy

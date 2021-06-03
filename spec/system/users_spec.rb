d15b9baa215e215549bdac870d3d3363f297c646
Sakai-yu0321 <on99.yu.sakai@gmail.com>
2021-06-03 10:57:28 +0900

    新規登録、ログインの結合テスト

---
+++ spec/system/users_spec.rb
@@ -0,0 +1,100 @@
+require 'rails_helper'
+
+RSpec.describe "ユーザー新規登録", type: :system do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  context 'ユーザー新規登録ができる時' do
+    it '正しい情報を入力すればユーザー新規登録ができてトップページに遷移する' do
+      # トップページに移動する
+      visit root_path
+      # トップページにサインアップページへ遷移するボタンがあることを確認する
+      expect(page).to have_content('新規登録')
+      # 新規登録ページへ移動する
+      visit new_user_registration_path
+      # ユーザー情報を入力する
+      fill_in 'nickname', with: @user.nickname
+      fill_in 'email', with: @user.email
+      fill_in 'password', with: @user.password
+      fill_in 'password_confirmation', with: @user.password_confirmation
+      # サインアップボタンを押すとユーザーモデルのカウントが1上がることを確認する
+      expect{
+        find('input[name="commit"]').click
+      }.to change { User.count }.by(1)
+      # トップページへ遷移したことを確認する
+      expect(current_path).to eq(root_path)
+      # トップページにログアウトボタンが表示されていることを確認する
+      expect(page).to have_content('ログアウト')
+      # サインアップページへ遷移するボタンや、ログインページへ遷移するボタンが表示されていないことを確認する
+      expect(page).to have_no_content('新規登録')
+      expect(page).to have_no_content('ログイン')
+    end
+  end
+  context 'ユーザー新規登録ができない時' do
+    it '誤った情報ではユーザー新規登録ができずに新規登録ページへ戻ってくる' do
+      # トップページに移動する
+      visit root_path
+      # トップページにサインアップページへ遷移するボタンがあることを確認する
+      expect(page).to have_content('新規登録')
+      # 新規登録ページへ移動する
+      visit new_user_registration_path
+      # ユーザー情報を入力する
+      fill_in 'nickname', with: ''
+      fill_in 'email', with: ''
+      fill_in 'password', with: ''
+      fill_in 'password_confirmation', with: ''
+      # サインアップボタンを押してもユーザーモデルのカウントは上がらないことを確認する
+      expect{
+        find('input[name="commit"]').click
+      }.to change { User.count }.by(0)
+      # 新規登録ページへ戻されることを確認する
+      expect(current_path).to eq user_registration_path
+    end
+  end
+end
+
+RSpec.describe 'ログイン', type: :system do
+  before do
+    @user = FactoryBot.create(:user)
+  end
+  it '保存されているユーザーの情報と合致すればログインができる' do
+    # トップページに移動する
+    visit root_path
+    # トップページにログインページへ遷移するボタンがあることを確認する
+    expect(page).to have_content('ログイン')
+    # ログインページへ遷移する
+    visit new_user_session_path
+    # 正しいユーザー情報を入力する
+    fill_in 'email', with: @user.email
+    fill_in 'password', with: @user.password
+    # ログインボタンを押す
+    find('input[name="commit"]').click
+    # トップページへ遷移することを確認する
+    expect(current_path).to eq(root_path)
+    # トップページにログアウトボタンが表示されていることを確認する
+    expect(page).to have_content('ログアウト')
+    # サインアップページへ遷移するボタンやログインページへ遷移するボタンが表示されていないことを確認する
+    expect(page).to have_no_content('新規登録')
+    # 「ログインしました」というフラッシュメッセージの要素を拾ってしまうので、結合テストを実行する場合はフラッシュメッセージを無効にする
+    expect(page).to have_no_content('ログイン')
+  end
+
+  context 'ログインできない時' do
+    it '保存されているユーザーの情報と合致しないとログインができない' do
+      # トップページに移動する
+      visit root_path
+      # トップページにログインページへ遷移するボタンがあることを確認する
+      expect(page).to have_content('ログイン')
+      # ログインページへ遷移する
+      visit new_user_session_path
+      # ユーザー情報を入力する
+      fill_in 'email', with: ''
+      fill_in 'password', with: ''
+      # ログインボタンを押す
+      find('input[name="commit"]').click
+      # ログインページへ戻されることを確認する
+      expect(current_path).to eq(new_user_session_path)
+    end
+  end
+end

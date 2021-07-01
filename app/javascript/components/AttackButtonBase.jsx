07e8327400a3709b2639f1639ef28928f9c3c325
Sakai-yu0321 <on99.yu.sakai@gmail.com>
2021-07-01 17:06:11 +0900

    ボタンをmapで繰り返し表示

--- app/javascript/components/AttackButtonBase.jsx
+++ app/javascript/components/AttackButtonBase.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const AButton = styled.button`
+const Buttons = styled.div`
+  width: 400px;
+  display: flex;
+  justify-content: space-around;
+`
+
+const Button = styled.button`
   border: none;
   background-color: #2a2a2a;
   padding: 20px;
@@ -15,10 +21,16 @@ const AButton = styled.button`
 
 export const AttackButtonBase = (props) => {
   const setWord = props.setWord;
+  const ACTIONS = props.ACTIONS;
   return (
-    <AButton onClick={() => setWord(props.reaction)}>
-      { props.children }
-    </AButton>
+    <Buttons>
+      {
+        ACTIONS.map((word, i) => {
+          return <Button onClick={() => setWord(word.reaction)}
+          key={i}>{word.action}</Button>
+        })
+      }
+    </Buttons>
   )
 }
 
